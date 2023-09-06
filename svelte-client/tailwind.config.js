679c0d51bf6e486c3f9181d63655d90d598fb070
max-ppv <7170119@gmail.com>
2023-09-06 10:56:35 +0400

    Initialize TailwindCSS for the project

---
+++ svelte-client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.svelte', './src/**/*.html'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
