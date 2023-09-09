c264078439cc31a35860d6301c01eddf6526c279
max-ppv <7170119@gmail.com>
2023-09-09 20:06:05 +0400

    Very nice!

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,14 +16,14 @@
   <meta property="og:url" content="{import.meta.env.VITE_BASE_URL}/about" />
 </svelte:head>
 
-<div class="text-center mb-6">
-  <img src="/logo.png" alt="Ticket Manager Logo" class="mb-4 w-32 mx-auto" />
-  <p class="text-gray-700 mb-4">
-    A simple service that powers an IT ticketing and issue management system.
+<div class="mb-6 text-center">
+  <img src="/logo.png" alt="Ticket Manager Logo" class="w-32 mx-auto mb-4" />
+  <p class="mb-4 text-gray-700">
+    A simple service that powers an IT ticketing and issue management system. Very nice!
   </p>
 </div>
 <a
   href="/"
-  class="bg-blue-500 text-white px-4 py-2 block mx-auto hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded"
+  class="block px-4 py-2 mx-auto text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
   >Back</a
 >
